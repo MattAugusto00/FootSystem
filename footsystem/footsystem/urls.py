4bdc31e1c239e42129874cacbdae1405315c6475
MattAugusto00 <58348794+MattAugusto00@users.noreply.github.com>
2023-11-23 20:10:29 -0300

    Delete footsystem directory

--- footsystem/footsystem/urls.py
+++
@@ -1,9 +0,0 @@
-from transfersapp import views
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('clubes/', views.clubes, name='listagem_clubes'),
-    path('admin/', admin.site.urls),
-]
--- footsystem/transfersapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Clube, Jogador, Transferencia
-
-admin.site.register(Clube)
-admin.site.register(Jogador)
-admin.site.register(Transferencia)
