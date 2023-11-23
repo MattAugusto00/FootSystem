25131a393bbb139fedfec456fcb796b337c331df
MattAugusto00 <58348794+MattAugusto00@users.noreply.github.com>
2023-11-23 20:12:06 -0300

    criação do projeto e criação da autenticação

---
+++ dev/crm/webapp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+
+# home
+def home(request):
+  return render(request, 'webapp/index.html')
+
+
+# register a user
+def register(request):
+  form = CreateUserForm()
+
+  if request.method == "POST":
+    form = CreateUserForm(request.POST)
+
+    if form.is_valid():
+      form.save()
+
+      return redirect('my-login')
+
+  context = {'form':form}
+
+  return render(request, 'webapp/register.html', context=context)
+
+
+# login a user
+
+def my_login(request):
+  form = LoginForm()
+  if request.method == "POST":
+    form = LoginForm(request, data=request.POST)
+
+    if form.is_valid():
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      user = authenticate(request, username=username, password=password)
+
+      if user is not None:
+        auth.login(request, user)
+        return redirect('dashboard')
+  context = {'form': form}
+  return render(request, 'webapp/my-login.html', context=context)
+
+
+# dashboard
+
+@login_required(login_url='my-login')
+def dashboard(request):
+  return render(request, 'webapp/dashboard.html')
+
+
+# user logout
+
+def user_logout(request):
+  auth.logout(request)
+  return redirect("my-login")
